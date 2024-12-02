dd740072a6053d39c163ec3fb76dc5992b9816fa
Stephan Raab <sarcartist@gmail.com>
2024-12-02 17:02:17 -0500

    added recursive version, but it's way more confusing to me

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -7,3 +7,15 @@ def iterativeReverseList(self, head: ListNode) -> ListNode:
         prev = curr
         curr = nxt
     return prev #aka the new head.
+
+def recursiveReverseList(self, head: ListNode) -> ListNode:
+    if not head:
+        return None
+
+    newHead = head
+    if head.next:
+        self.recursiveReverseList(head.next)
+        head.next.next = head
+    head.next = None
+    
+    return newHead
