e7d15b8408c5004600b640d545fdd71f19dffe8a
Stephan Raab <sarcartist@gmail.com>
2024-12-02 16:37:11 -0500

    iterative reverse linked list

---
+++ reverse_linked_list.py
@@ -0,0 +1,9 @@
+def iterativeReverseList(self, head: ListNode) -> ListNode:
+    prev, curr = None, head
+
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+    return prev #aka the new head.
