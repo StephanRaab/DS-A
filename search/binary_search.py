16ecf521bb7b6276024e95541063c46559fd643e
Stephan Raab <sarcartist@gmail.com>
2024-12-02 16:06:19 -0500

    use different formula for center to avoid int overload

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,11 +1,10 @@
 arr = [1,2,3,3,3,4,5,6,7,8,9]
 
 def binary_search(arr, target):
-    l = 0
-    r = len(arr) - 1
+    l,r = 0, len(arr)-1
 
-    while l <= r:
-        m = (l + r) // 2
+    while l <= r:        
+        m = l + (r - l) // 2
 
         if target > arr[m]:
             l = m + 1
