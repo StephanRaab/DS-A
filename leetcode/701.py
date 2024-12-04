120dd779d5cb204d71d30622b04bcc91d6ab238d
Stephan Raab <sarcartist@gmail.com>
2024-12-04 13:02:03 -0500

    simplify 701 and add iterative solution

--- leetcode/701.py
+++ leetcode/701.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# this is the recursive solution, which is O(h) (h for height)
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if not root:
@@ -11,7 +13,11 @@ class Solution:
 
         if val > root.val:
             root.right = self.insertIntoBST(root.right, val)
-        elif val < root.val:
+        else:
             root.left = self.insertIntoBST(root.left, val)
         
         return root
+
+# iterative solution
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
