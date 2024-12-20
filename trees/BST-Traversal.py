78a8a440a9759ef81f2b145aa679fc28f0a554c3
Stephan Raab <sarcartist@gmail.com>
2024-12-20 13:19:42 -0500

    adding pre and post-order

--- trees/BST-Traversal.py
+++ trees/BST-Traversal.py
@@ -6,3 +6,16 @@ def inorder(root):
     print(root.val)
     inorder(root.right)
     
+def preorder(root):
+    if not root:
+        return
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+def postorder(root):
+    if not root:
+        return
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
