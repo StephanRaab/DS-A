f8170f803a095840819e3489505d5e7d2a29d372
Stephan Raab <sarcartist@gmail.com>
2024-12-22 22:37:55 -0500

    removing comments

--- leetcode/144.cs
+++ leetcode/144.cs
@@ -1,17 +1,5 @@
 // 144. Binary Tree Preorder Traversal
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 public class Solution {
     List<int> res = new List<int>();
 
--- leetcode/94.cs
+++ leetcode/94.cs
@@ -1,16 +1,5 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// 94. Binary Tree Inorder Traversal
+
 public class Solution {
     List<int> res = new List<int>();
 
