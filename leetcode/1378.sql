72a7054020f9571e4f0fcae65d06fb14edc381e3
Stephan Raab <sarcartist@gmail.com>
2024-12-25 22:37:05 -0500

    name simplification

--- leetcode/1378.sql
+++ leetcode/1378.sql
@@ -2,3 +2,8 @@
 SELECT EmployeeUNI.unique_id, Employees.Name
 FROM Employees
 LEFT JOIN EmployeeUNI ON EmployeeUni.id = Employees.id
+
+-- simplifying the names
+SELECT eu.unique_id, e.Name
+FROM Employees e
+LEFT JOIN EmployeeUNI eu ON eu.id = e.id
