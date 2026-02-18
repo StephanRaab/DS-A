6333d5d40e84caf26c7dffe4b4733f129c7c800a
Stephan Raab <sarcartist@gmail.com>
2026-02-17 21:22:49 -0500

    optimized twosum

--- leetcode/1.js
+++ leetcode/1.js
@@ -1,9 +1,42 @@
-twoSum(nums, target) {
-        for (let i = 0; i < nums.length; i++){
-            for (let j = i+1; j < nums.length;j++){
-                if (nums[i] + nums[j] === target){
-                    return [i, j];
-                }
+function twoSumBruteForce(nums, target) {
+    for (let i = 0; i < nums.length; i++){
+        for (let j = i+1; j < nums.length;j++){
+            if (nums[i] + nums[j] === target){
+                return [i, j];
             }
         }
     }
+}
+
+
+/*
+[5,4,6] => target 10
+
+map = {
+    5: 0,
+    4: 1,
+    6: 2
+}
+
+map[4] returns the value, which in this case is the INDEX
+
+
+*/
+
+
+function twoSumOptimization(nums, target){
+    const numberToIndex = {};
+
+    // build the map
+    for (let i = 0; i < nums.length; i++){
+        numberToIndex[nums[i]] = i;
+    }
+
+    for (let i = 0; i < nums.length; i++){
+        let targetNum = target - nums[i];
+        // if not undefined and can't be the same as the looping index
+        if (numberToIndex[targetNum] !== undefined && numberToIndex[targetNum] !== i) {
+            return [i, numberToIndex[targetNum]]
+        }
+    }
+}
