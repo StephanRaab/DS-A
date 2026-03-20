dec2ebd36d7e02ac6464cf3deb6f236f18b2f0d8
Stephan Raab <sarcartist@gmail.com>
2026-03-19 21:13:39 -0400

    structy longest word

---
+++ structy/1.intro/longest_word.py
@@ -0,0 +1,13 @@
+# longest word
+# Write a function, longest_word, that takes in a sentence string as an argument. The function should return the longest word in the sentence. 
+# If there is a tie, return the word that occurs later in the sentence.
+
+def longest_word(sentence):
+    split = sentence.split(" ")
+    longest = ""
+
+    for word in split:
+        if len(word) > len(longest):
+            longest = word
+
+    return longest
