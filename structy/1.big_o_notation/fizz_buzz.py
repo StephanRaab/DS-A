eed22a55e91cf24fb051a2680938362c3f67b908
Stephan Raab <sarcartist@gmail.com>
2026-03-22 11:59:55 -0400

    add structy fizz buzz

---
+++ structy/1.big_o_notation/fizz_buzz.py
@@ -0,0 +1,14 @@
+def fizz_buzz(n):
+    res = []
+
+    for i in range(1, n+1):
+        if i % 3 == 0 and i %% 5 == 0:
+            res.append("fizzbuzz")
+        elif i % 3 == 0:
+            res.append("fizz")
+        elif i % 5 == 0:
+            res.append("buzz")
+        else:
+            res.append(i)
+
+    return res
